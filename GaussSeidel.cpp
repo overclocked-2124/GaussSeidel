b16a19e91e2377ac34b79043a8c18aaae511e9f6
Sumukha_Upadhyaya <146051934+overclocked-2124@users.noreply.github.com>
2024-10-24 23:47:23 +0530

    added initial code

---
+++ GaussSeidel.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<math.h>
+#include<iomanip>
+
+int main(){
+
+    float a[9],t,x,y,z;
+    a[0]=0;
+    x=0;
+    y=0;
+    z=0;
+    
+    std::cout<<"enter the matric elements after making it in diagonally dominant order: ";
+    for(int i=1;i<9;i++){
+        std::cin>>a[i]
+    }
+
+    std::cout<<"enter the number of approximations to be done:";
+    std::cin>>t;
+
+    for (int i=0;i<t;i++)){
+
+
+    }
+
+
+    return(0);
